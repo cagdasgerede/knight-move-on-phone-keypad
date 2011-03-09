e3cefdeb3b5146cfd5370ed0aaf6da1453e648dc
vrgr <cagdas.gerede@gmail.com>
2011-03-09 19:03:02 +0200

    updated README

--- chess.rb
+++ chess.rb
@@ -1,18 +1,18 @@
 
 
-Arr=[[4,6],[6,8],[7,9],[4,8],[3,9],[],[1,7],[2,6],[1,3],[2,4]]
+Move_map=[[4,6],[6,8],[7,9],[4,8],[3,9],[],[1,7],[2,6],[1,3],[2,4]]
 
 def compute(v,n)
 	h = 1
-	queue = [ "#{v}" ]
+	prefix_queue = [ "#{v}" ]
 	cur_level = [ v ]
 	new_level = []
 	while h <= n
 		cur_level.each do |k|
-			prefix = queue.shift
-			Arr[ k ].each do |x|
+			prefix = prefix_queue.shift
+			Move_map[ k ].each do |x|
 				new_level.push x
-				queue.push "#{prefix},#{x}"
+				prefix_queue.push "#{prefix},#{x}"
 			end
 		end
 		cur_level = new_level
@@ -20,8 +20,8 @@ def compute(v,n)
 		h += 1
 	end
 
-	puts queue
+	puts prefix_queue
 	puts 'done'
 end
 
-compute(1, ARGV.first.to_i)
+compute( ARGV[0].to_i, ARGV[1].to_i)
